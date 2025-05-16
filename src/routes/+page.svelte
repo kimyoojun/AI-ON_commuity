10d815018c0a90a141268b623887a18cca855763
kimyoojun <anano3134@gmail.com>
2025-05-16 18:51:10 +0900

    커뮤니티 상단바 뼈대 구현

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,81 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id="top_bar">
+    <h1 id="title">고양이</h1>
+    <input placeholder="검색어를 입력하세요" id="search"/>
+
+    <div id="signin">
+        <input placeholder="아이디를 입력하세요" type="text" class="sign_in"/>
+        <input placeholder="비밀번호를 입력하세요" type="password" class="sign_in"/>
+        <input placeholder="전화번호를 입력하세요" type="number" class="sign_in"/>
+        <button id="btn">로그인</button>
+    </div>
+</div>
+
+<ol>
+    <li>고양이가 귀여운 이유</li>
+    <li>고양이 종류</li>
+    <li>고양이 성격</li>
+    <li>고양이가 좋아하는 음식</li>
+</ol>
+
+<ul>
+    <li>고양이가 귀여운 이유</li>
+    <li>고양이 종류</li>
+    <li>고양이 성격</li>
+    <li>고양이가 좋아하는 음식</li>
+</ul>
+
+<p>오리너구리(학명: Ornithorhynchus anatinus)는 오스트레일리아와 태즈메이니아섬 토종의 반수서성 <strong>단공류(單孔類)</strong> 포유류의 일종이다. <br>가시두더지 4종과 함께 현존하는 다섯뿐인 단공류이며, 가장 원시적인 포유류인 동시에 난생(卵生)의 번식 방법을 택하고 있는 극소수의 포유류 중 하나이다. 또한 오리너구리속, 오리너구리과에서도 유일하게 현재까지 명맥을 잇고 있는 종이자 모식종으로, 같은 오리너구리과에 속하는 것으로 여겨지는 다른 여러 종은 모두 화석으로만 발견된다. 조류와 혼동되거나 조류와 포유류의 중간종이라는 오해를 받으나, 실제 유전적으로는 조류보다 파충류에 더 근접한 포유류이다. 오리를 닮은 부리, 비버를 닮은 꼬리, 수달을 닮은 발을 가진 다소 우스꽝스러운 외모에 알을 낳는 생태까지 겹쳐, 서구 박물학자들은 살아 있는 오리너구리를 확인하기 전까지, 1799년 학계에 기증된 오리너구리의 표본을 가리켜 다른 여러 동물들의 부위를 뒤섞어 조작해 놓은 가짜 표본이라고 의혹을 제기한 바도 있었다.</p>
+
+
+
+<a href="https://www.naver.com/">다음</a>
+
+오리너구리(학명: Ornithorhynchus anatinus)는<br> 오스트레일리아와 태즈메이니아섬 토종의 반수서
+
+
+<img src="고양이.jpg" alt="고양이사진">
+
+<select>
+    <option>고양이 기분 상태</option>
+    <option>기쁨</option>
+    <option>화남</option>
+    <option>슬픔</option>
+</select>
+
+<style>
+    #title {
+        width: 150px;
+        height: 100px;
+        padding: 20px;
+        border: white 5px solid;
+        border-radius: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #top_bar {
+        width: 100%;
+        height: 200px;
+        background-color: blue;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    #search {
+        width: 250px;
+        height: 50px;
+    }
+
+    .sign_in {
+        width: 150px;
+        height: 20px;
+    }
+
+    #signin {
+        display: flex;
+        flex-direction: column;
+    }
+
+</style>
