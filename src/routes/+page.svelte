6d105340d953be3558b1ca5b85836676d5c39408
ThefastestmaninChangwon <atopbbb@gmail.com>
2025-05-16 18:50:08 +0900

    커뮤니티 상단바 구현현

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,74 @@
-<h1>안녕</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id= top_bar>
+    <h1 id = car>자동차</h1>
+    <input id = c1 placeholder="안녕하다" type="text">
+    <div id = main>
+        <input placeholder="안녕하다" type="number">
+        <input placeholder="안녕하다" type='text'>
+        <input placeholder="안녕하다" type="password">
+        <button>로그인</button>
+    </div>
+</div>
+
+
+<ol>
+    <li>안녕</li>
+    <li>네 안녕하세요</li>
+    <il>하이</il>
+</ol>
+
+<ul> 
+    <li>안녕</li>
+    <li>네 안녕하세요</li>
+    <il>하이</il>
+</ul>
+<p> 가<br><strong> 다라마바사아자</strong>나다라마바사아자</p>
+
+
+
+<a href="https://www.naver.com/">네이버</a>
+<img src= '맥라렌p1.jpg' alt="맥라렌 p1 사진">
+
+<select>
+
+    <option>어디가실건가요?</option>
+    <option>서울</option>
+    <option>부산</option>
+    <option>경남</option>
+    <option>인천</option>
+</select>
+
+<style>
+    #car {
+        width: 150px;
+        color: blue;
+        background-color: crimson;
+        margin: 50px;
+        padding: 20px;
+        border: midnightblue solid 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    
+    
+    }
+
+    #top_bar{
+        width: 100%;
+        display: flex;
+        background-color: aquamarine;
+        align-items: center;
+        justify-content: space-between;
+    }
+    #c1{
+        margin: 0px;
+        padding: 10px;
+        width: 300px;
+        height: 20px;
+    }
+    #main{
+        display: flex;
+        flex-direction: column;
+        margin: 50px;
+    }
+
+</style>
