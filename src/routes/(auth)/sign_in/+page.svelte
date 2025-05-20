64e4f82617d5ecd3ca7b6ce36862d836eb9fa1a4
kimyoojun <anano3134@gmail.com>
2025-05-20 17:33:59 +0900

    로그인 페이지 생성 및 로그인 요소 추가

---
+++ src/routes/(auth)/sign_in/+page.svelte
@@ -0,0 +1,88 @@
+<div id="main_box">
+    <div id="top_bar">
+        <h1 id="title_rogo">
+            <a href="/" id="title_a">AI:ON</a>
+        </h1>
+    </div>
+    <div id="body">
+        <div id="login_box">
+            <h1 id="login_text">로그인</h1>
+            <p id="welcome_text">개발자들을 위한 AI:ON 멤버 커뮤니티에 오신것을 <br/>환영합니다.</p>
+            <input placeholder="이메일 주소" class="login_input"/>
+            <input placeholder="비밀번호" class="login_input"/>
+            <button id="login_btn">로그인</button>
+        </div>
+    </div>
+</div>
+
+<style>
+    #main_box {
+        width: 100%;
+        height: 100%;
+    }
+
+    #top_bar {
+        width: 100%;
+        height: 100px;
+    }
+
+    #title_rogo {
+        margin-left: 15px;
+    }
+
+    #title_a {
+        text-decoration-line: none;
+        color: white;
+    }
+
+    #body {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    #login_box {
+        width: 460px;
+        height: 737px;
+        border: 3px solid gray;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    #login_text {
+        width: 100%;
+        text-align: center;
+    }
+
+    #welcome_text {
+        width: 100%;
+        text-align: center;
+    }
+
+    .login_input {
+        width: 310px;
+        height: 24px;
+        padding: 12px 16px;
+        border: 2px solid gray;
+        border-radius: 10px;
+        margin: 5px 0;
+    }
+
+    #login_btn {
+        width: 345px;
+        height: 50px;
+        padding: 14px 18px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 2px solid gray;
+        border-radius: 10px;
+        margin: 10px 0;
+    }
+</style>
